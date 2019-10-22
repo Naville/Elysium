f4c02144e31e488a847915c1d6440c05286ba4ef
Zhang <5205699+Naville@users.noreply.github.com>
2019-10-22 16:50:18 +0800

    Update Dash.m

--- Dash.m
+++ Dash.m
@@ -1,6 +1,7 @@
 
 //clang -shared -undefined dynamic_lookup -o /Applications/Dash.app/Contents/MacOS/libDash.dylib Dash.m
 //optool install -c load -p @executable_path/libDash.dylib -t /Applications/Dash.app/Contents/MacOS/Dash
+//codesign --remove-signature /Applications/Dash.app/Contents/MacOS/Dash
 //Fucking Stupid License Model
 //10/10 will definitely not purchase again
 #import <objc/runtime.h>
